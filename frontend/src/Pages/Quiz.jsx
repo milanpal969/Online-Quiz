d846a44f9786b8a08f6c2e390e3491b201e5e978
milanpal969 <palmilan568@gmail.com>
2024-10-03 00:48:28 +0530

    added display questions

--- frontend/src/Pages/Quiz.jsx
+++ frontend/src/Pages/Quiz.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getCurrent } from './ApiCalls';
-
+import { getAllQuestions } from '../componets/getAllQuestions';
 function Quiz() {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [error, setError] = useState(null);
+    const [data,setdata] = useState([]);
+    const [loading,setloading] = useState(false);
 
 
     const handlePermission = () => {
@@ -39,6 +41,22 @@ function Quiz() {
         };
 
         fetchCurrentUser();
+
+        const getallData = async () => {
+            setloading(true);
+            try{
+                const response  = await getAllQuestions();
+                if(response.data.data){
+                    setloading(false);
+                    setError(null);
+                    setdata(response.data.data);
+                }
+            }catch(e){
+                setloading(false);
+                setError(e);
+            }
+        }
+        getallData();
     }, [navigate]);  
 
     const handleLogout = () => {
@@ -56,6 +74,31 @@ function Quiz() {
             
             {user ? <h2>Welcome, <span style={{cursor:"pointer"}} onClick={handlePermission}>{user.name}</span></h2> : <p>Loading user data...</p>}
             <button onClick={handleLogout}>Log Out</button>
+
+            {loading ? "Loading... " : 
+            
+            <div>
+
+                {
+                    data.map((question,index)=><div key={index}>
+                        <div style={{display:"flex", marginBottom:"10px", gap:"10px"}}>
+                          <span>{index+1}</span>  
+                          <div>{question.question}</div>
+                        </div>
+                        <div style={{width:"fit-content"}}>
+                        {
+                            question.options.map((opt,idx)=><div style={{padding:"10px", border:"solid 2px grey"}} key={idx}>
+                               <span>{String.fromCharCode('A'.charCodeAt(0) + idx)}</span>. {opt}
+                                </div>)
+                        }
+                        </div>
+                        </div>)
+                }
+                
+            </div>
+            
+            
+            }
         </div>
     );
 }
